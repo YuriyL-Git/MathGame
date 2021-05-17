81301f66a6ce3ef7a20df593b46ee348ee1e94e9
Yuriy Lipchey <xaosmmm@gmail.com>
2021-05-17 19:55:48 +0300

    refactor: replace selector with document.body

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { App } from './app/app';
 import { Router } from './app/components/router/router';
 
 window.onload = async () => {
-  const appElement = document.getElementById('app') as HTMLElement;
+  const appElement = document.body;
   const app = new App(appElement);
   const router = new Router(app);
   await app.start();
